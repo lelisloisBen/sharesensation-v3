49879593e26f5285854e479bbbf279234b790b6e
lelisloisBen <samirbenzada@gmail.com>
2021-01-16 17:09:28 -0500

    update

--- src/models.py
+++ src/models.py
@@ -39,3 +39,20 @@ class users(db.Model):
             "phone": self.phone,
             "admin": self.admin
         }
+
+class activities(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cat =  db.Column(db.String(120))
+    name = db.Column(db.String(120))
+    path = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<activities %r>' % self.cat
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "cat": self.cat,
+            "name": self.name,
+            "path": self.path
+        }
