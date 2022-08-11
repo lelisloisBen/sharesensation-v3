91faeac28bda2a55bc8f83a4e5086c6101f80b4b
yevhenii <snape793@gmail.com>
2022-08-12 05:19:57 +0800

    enable debug

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ class Config(object):
     }
 
 class ProductionConfig(Config):
-    DEBUG = False
+    DEBUG = True
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
