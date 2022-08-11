8a58b60988880192a4116317a940959cd76b2915
yevhenii <snape793@gmail.com>
2022-08-12 00:29:22 +0800

    change db

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SECRET_KEY = '57e19ea558d4967a552d03deece34a70'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('JAWSDB_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
     # Social Auth
     OAUTH_CREDENTIALS = {
