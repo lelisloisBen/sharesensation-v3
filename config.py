68b20910eba35c915edfa957492c20b0db342fbe
yevhenii <snape793@gmail.com>
2022-08-11 22:02:13 +0800

    add message tls setting

--- config.py
+++ config.py
@@ -32,6 +32,7 @@ class ProductionConfig(Config):
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FRONTEND_URL = 'https://sharesensation.herokuapp.com'
