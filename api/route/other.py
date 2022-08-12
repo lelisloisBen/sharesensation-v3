8c14b93fde94c7c3e3776a2a574f49791812f293
yevhenii <snape793@gmail.com>
2022-08-12 12:58:58 +0800

    test

--- api/route/other.py
+++ api/route/other.py
@@ -15,9 +15,13 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from functools import wraps
 from database.model.Activity import Activity
 from database.model.SaleTax import SaleTax
+import logging
+
+logger = logging.getLogger(__name__)
 
 @app.route('/activities', methods=['GET'])
 def handle_activities():
+    logger.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
     if request.method == 'GET':
         allActivities = Activity.query.all()
