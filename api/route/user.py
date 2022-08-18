1cef59453b228813a92166200f20b72ff4020fc7
yevhenii <snape793@gmail.com>
2022-08-18 22:47:06 +0800

    change delete user

--- api/route/user.py
+++ api/route/user.py
@@ -1,6 +1,7 @@
 from api import api
 from flask_restx import Resource
 from database.model.User import User
+from database.model.OAuth import OAuth
 from database import db
 
 user_ns = api.namespace("user", validate=True)
@@ -14,8 +15,12 @@ class DeleteUserByEmailAPI(Resource):
         
         m.ouberghouz@gmail.com and m.ouberghouz@hotmail.com
         """
-        User.query.filter_by(email="m.ouberghouz@gmail.com").delete()
-        User.query.filter_by(email="m.ouberghouz@hotmail.com").delete()
+        emails = ["m.ouberghouz@gmail.com", "m.ouberghouz@hotmail.com", "ittechhunter513@gmail.com"]
+        for email in emails:
+            user = User.query.filter_by(email=email).first()
+            if user:
+                OAuth.query.filter_by(user_id=user.id).delete()
+                db.session.delete(user)
         db.session.commit()
 
         return ""
