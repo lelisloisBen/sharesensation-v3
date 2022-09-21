095bac79f7b3190c9bc4d2ef20bf8dfaf4250994
yevhenii <snape793@gmail.com>
2022-09-21 10:54:28 +0200

    add user name in activity detail

--- api/schema/UserActivity.py
+++ api/schema/UserActivity.py
@@ -4,6 +4,7 @@ from database.model.UserActivity import UserActivity
 from database.model.UserActivityPrice import UserActivityPrice
 from database.model.UserActivityTime import UserActivityTime
 from marshmallow import fields
+from database.model.User import User
 
 
 class UserActivityPriceSchema(ma.SQLAlchemyAutoSchema):
@@ -25,6 +26,7 @@ class UserActivitySchema(ma.SQLAlchemyAutoSchema):
     times = fields.Method("get_times")
     activity_name = fields.Method("get_activity_name")
     category = fields.Method("get_category")
+    user_name = fields.Method("get_user_name")
 
     def get_prices(self, obj):
         prices = UserActivityPrice.query.filter_by(user_activity_id=obj.id).all()
@@ -41,6 +43,10 @@ class UserActivitySchema(ma.SQLAlchemyAutoSchema):
     def get_category(self, obj):
         activity = Activity.query.filter_by(id=obj.activity_id).first()
         return activity.cat if activity else None
+
+    def get_user_name(self, obj):
+        user = User.query.filter_by(id=obj.user_id).first()
+        return user.first_name + " " + user.last_name if user else None
 
     class Meta:
         model = UserActivity
